9af3d5181d7a8859540f62668375211fedf90d5e
TLLong <tllongdev@gmail.com>
2020-01-26 21:28:23 -0600

    Models Completed

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,36 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('Student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    lowercase: true,
+    validate: {
+      isEmail: true,
+      notEmpty: true
+    }
+  }
+});
+
+Student.hook('beforeCreate', (student) => {
+  student.firstName = student.firstName.replace(/^./g, c => c.toUpperCase());
+  student.lastName = student.lastName.replace(/^./g, c => c.toUpperCase());
+});
 
 module.exports = Student;
--- db/models/test.js
+++ db/models/test.js
@@ -3,6 +3,23 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
+const Test = db.define('Test', {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+});
+
+Test.belongsTo(Student, { as: 'student' });
 
 module.exports = Test;
